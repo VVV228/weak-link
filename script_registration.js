d9e047696e9dcc51e038c13b0f3ad5b0f0b493bf
k0narro <natalya.tereshko.nt@gmail.com>
2023-11-02 14:25:37 +0200

    added text style

--- script_registration.js
+++ script_registration.js
@@ -1,6 +1,10 @@
 const forms = document.querySelector(".forms"),
       pwShowHide = document.querySelectorAll(".eye-icon"),
       links = document.querySelectorAll(".link");
+      passwordFields = document.querySelectorAll(".password");
+    //   message = document.querySelector(".message");
+
+      let passwordsMatch = false;
 
 pwShowHide.forEach(eyeIcon => {
     eyeIcon.addEventListener("click", () => {
@@ -23,5 +27,27 @@ links.forEach(link => {
     link.addEventListener("click", e => {
        e.preventDefault();
        forms.classList.toggle("show-signup");
+    //    message = document.querySelector(".message");
     })
 })
+
+// // Додамо подію для перевірки паролів
+// passwordFields[1].addEventListener("input", () => {
+//     if (passwordFields[0].value === passwordFields[1].value) {
+//         passwordsMatch = true;
+//         // Повідомлення про співпадання паролів
+//         message.textContent = "Паролі співпадають";
+//     } else {
+//         passwordsMatch = false;
+//         // Повідомлення про невідповідність паролів
+//         message.textContent = "Паролі не співпадають";
+//     }
+// });
+
+// // Додамо подію для натискання кнопки "Зареєструватись"
+// registerButton.addEventListener("click", (e) => {
+//     if (!passwordsMatch) {
+//         e.preventDefault(); // Блокуємо перехід, якщо паролі не співпадають
+//         message.textContent = "Будь ласка, виправте помилку в паролях.";
+//     }
+// });
