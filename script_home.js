27022faf27823bfe59d0c97f516dc35fb6b16ba3
MrCurly <MY_NAME@example.com>
2023-11-26 20:35:02 +0200

    Add some jQuery

--- script_home.js
+++ script_home.js
@@ -1,11 +1,4 @@
-// $(function(){
-//     $(".step-box").slideUp(1000).delay(1000).slideDown(1000);
-// });
-
-
-// $(".options").mouseenter(function(){
-//     alert("You entered p1!");
-//   });
-
-
+$(function(){
+    $(".step-box").slideUp(1000).delay(1000).slideDown(1000);
+});
 
